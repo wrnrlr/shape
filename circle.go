e59e0651e14c6f62165b6f50741729faf5fbb62d
Werner Laurensse <werner@alman.ax>
2020-01-18 21:44:29 +0100

    Add stack op to circle

--- circle.go
+++ circle.go
@@ -3,7 +3,10 @@ package shape
 import (
 	"gioui.org/f32"
 	"gioui.org/layout"
+	"gioui.org/op"
 	"gioui.org/op/clip"
+	"gioui.org/op/paint"
+	"image/color"
 )
 
 const c = 0.55228475 // 4*(sqrt(2)-1)/3
@@ -13,7 +16,7 @@ type Circle struct {
 	Radius float32
 }
 
-func (cc Circle) Stroke(lineWidth float32, gtx *layout.Context) f32.Rectangle {
+func (cc Circle) Stroke(col color.RGBA, lineWidth float32, gtx *layout.Context) f32.Rectangle {
 	scale := lineWidth // base value on lineWidth and r
 
 	r := cc.Radius
@@ -21,6 +24,9 @@ func (cc Circle) Stroke(lineWidth float32, gtx *layout.Context) f32.Rectangle {
 	p := cc.Center
 	box := f32.Rectangle{Min: f32.Point{X: p.X - r, Y: p.Y - r}, Max: f32.Point{X: p.X + r, Y: p.Y + r}}
 
+	var stack op.StackOp
+	stack.Push(gtx.Ops)
+	paint.ColorOp{col}.Add(gtx.Ops)
 	var path clip.Path
 	path.Begin(gtx.Ops)
 	path.Move(f32.Point{X: p.X + w, Y: p.Y + r})
@@ -42,13 +48,19 @@ func (cc Circle) Stroke(lineWidth float32, gtx *layout.Context) f32.Rectangle {
 	path.Cube(f32.Point{X: 0, Y: -r * c}, f32.Point{X: -(r - r*c), Y: -r}, f32.Point{X: -r, Y: -r}) // NE
 	path.Cube(f32.Point{X: -r * c, Y: 0}, f32.Point{X: -r, Y: r - r*c}, f32.Point{X: -r, Y: r})     // NW
 	path.End().Add(gtx.Ops)
+	path.End().Add(gtx.Ops)
+	//paint.PaintOp{f32.Rectangle{Max:f32.Point{w,h}}}.Add(gtx.Ops)
+	paint.PaintOp{box}.Add(gtx.Ops)
+	stack.Pop()
 	return box
 }
 
-func (cc Circle) Fill(gtx *layout.Context) f32.Rectangle {
+func (cc Circle) Fill(col color.RGBA, gtx *layout.Context) f32.Rectangle {
+	p := cc.Center
 	r := cc.Radius
 	w, h := r*2, r*2
-	p := cc.Center
+	var stack op.StackOp
+	stack.Push(gtx.Ops)
 	var path clip.Path
 	path.Begin(gtx.Ops)
 	path.Move(f32.Point{X: p.X, Y: p.Y})
@@ -58,6 +70,10 @@ func (cc Circle) Fill(gtx *layout.Context) f32.Rectangle {
 	path.Cube(f32.Point{X: 0, Y: -r * c}, f32.Point{X: r - r*c, Y: -r}, f32.Point{X: r, Y: -r})   // NW
 	path.Cube(f32.Point{X: r * c, Y: 0}, f32.Point{X: r, Y: r - r*c}, f32.Point{X: r, Y: r})      // NE
 	path.End().Add(gtx.Ops)
-	box := f32.Rectangle{Min: f32.Point{X: p.X - r, Y: p.Y - r}, Max: f32.Point{X: p.X + r, Y: p.Y + r}}
+	box := f32.Rectangle{Max: f32.Point{X: p.X + w, Y: p.Y + h}}
+	//paint.PaintOp{f32.Rectangle{Max:f32.Point{w,h}}}.Add(gtx.Ops)
+	paint.ColorOp{col}.Add(gtx.Ops)
+	paint.PaintOp{box}.Add(gtx.Ops)
+	stack.Pop()
 	return box
 }
